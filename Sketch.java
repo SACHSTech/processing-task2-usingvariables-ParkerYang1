8e0cc09dcfbdf11fe50723d359354d0cbb96e666
Parker Yang <99832581+ParkerYang1@users.noreply.github.com>
2022-02-25 03:01:20 +0000

    Done

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(100, 100);
   }
 
   /** 
@@ -16,21 +16,41 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(71, 218, 255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    fill(0, 128, 0);
+    rect(0, height/1.6f, width, height/1.6f);
+    
+    fill(200, 155, 125);
+    rect(width/8, height/2.6f, width/2.6f, height/2.6f); 
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    fill(125, 65, 45);
+    rect(width/6.6f, height/4.4f, width/16, height/6.6f);
+    
+    fill(125, 65, 45);
+    triangle(width/8, height/2.6f, width/2, height/2.6f, width/3.2f, height/5);
+
+    fill(255, 255, 0);
+    ellipse(width/1.2f, height/6.6f, width/4, height/4);
+
+    fill(255, 197, 143);
+    rect(width/3.2f, height/1.9f, width/6.6f, height/4.4f);
+
+    fill(255, 255, 255);
+    rect(width/6.6f, height/1.9f, width/8, height/8);
+
+    fill(0, 0, 0);
+    line(width/4.7f, height/1.9f, width/4.7f, height/1.5f);
+
+    fill(0, 0, 0);
+    line(width/6.6f, height/1.7f, width/3.6f, height/1.7f);
+
+    fill(0, 0, 0);
+    ellipse(width/2.3f, height/1.5f, width/26.6f, height/26.6f);
   }
-  
-  // define other methods down here.
 }
